d15a62e426c4b85042bee8de2e985b6725d2d134
mluckeroth <mluckeroth@gmail.com>
2019-04-02 21:40:56 -0700

    completed adding static files

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import stub_view
+from .views import stub_view, list_view, detail_view
 
 
 urlpatterns = [
     path('',
-        stub_view,
+        list_view,
         name="blog_index"),
+    path('posts/<int:post_id>/', detail_view, name='blog_detail'),
 ]
 
